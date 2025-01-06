1ca317faa0212506285c05fac9f2c859dabbb4a0
Putra Satria Nagara Mahmud <167815376+AgathaOnGithub@users.noreply.github.com>
2025-01-06 08:28:18 +0700

    Add files via upload

---
+++ MovieController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Movie;
+use App\Models\Genre;
+use Illuminate\Http\Request;
+
+class MovieController extends Controller
+{
+    public function index()
+    {
+        $movies = Movie::with('genre')->get();
+        return view('movies.index', compact('movies'));
+    }
+
+
+    public function create()
+    {
+        $genres = Genre::all();
+        return view('movies.create', compact('genres'));
+    }
+
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required|string|max:255',
+            'synopsis' => 'required',
+            'year' => 'required|digits:4',
+            'genre_id' => 'required|uuid',
+        ]);
+
+        Movie::create($request->all());
+
+        return redirect()->route('movies.index')->with('success', 'Movie created successfully.');
+    }
+    public function edit($id)
+{
+    $movie = Movie::findOrFail($id);
+    $genres = Genre::all();
+    return view('edit', compact('movie', 'genres'));
+}
+
+}
